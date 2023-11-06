090e3e1209127107d518aadc9fa447c72bc4118b
Albert Karagulov <albert.karagulov@mail.ru>
2023-11-06 16:15:51 +0300

    created chat component and chat page

--- src/pages/TouristChat/index.jsx
+++ src/pages/TouristChat/index.jsx
@@ -1,10 +1,12 @@
 import styles from './TouristChat.module.scss';
 import { NavTourist } from '../../components/NavTourist';
+import { ChatBox } from '../../components/ChatBox';
 
 export const TouristChat = () => {
   return (
-    <div>
+    <div className={styles.tourist_chat}>
       <NavTourist />
+      <ChatBox />
     </div>
   );
 };
